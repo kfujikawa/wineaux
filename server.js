f13f96c413a9cfcdbcc43826e31443102162c0e2
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-03-29 19:02:43 -0700

    revert closeServer changes and update tests again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
   saveUninitialized: false,
   secret: "Shh, its a secret!",
   store: new MongoStore({
-    url:     url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
 ,
     touchAfter: 24 * 3600
   })
